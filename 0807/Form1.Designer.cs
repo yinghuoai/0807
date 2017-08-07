d64213ae0765a8e0bd253e91721c180d2e8b0eaf
yinghuoai <964549765@qq.com>
2017-08-07 18:08:01 +0800

    new简单的串口发送数据

--- 0807/Form1.Designer.cs
+++ 0807/Form1.Designer.cs
@@ -29,11 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(305, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "数据";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 386);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- 0807/Form1.cs
+++ 0807/Form1.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/********************************************************************************
+** Company： 
+** auth：    jerryli
+** date：    2017/8/7 17:08:28
+** desc：    简单的串口发送数据操作
+** Ver.:     V1.0.0
+*********************************************************************************/
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +25,69 @@ namespace _0807
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str;
+            for(int i = 0;i <256; i++)
+            {
+                str = i.ToString("x").ToUpper();
+                if(str.Length == 1)
+                {
+                    str = "0" + str;
+             
+                }
+                comboBox1.Items.Add("0x" + str);
+
+
+
+            }
+
+            comboBox1.Text = "0X00";//初始值
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = comboBox1.Text;
+            string convertdata = data.Substring(2, 2);
+            byte[] buffer = new byte[1];//因为write（）中需要数组参数
+            buffer[0] = Convert.ToByte(convertdata, 16);//将字符串转化为byte型变量
+            try//防止出错
+            {
+                serialPort1.Open();
+                serialPort1.Write(buffer,0,1);
+                serialPort1.Close();
+
+
+            }
+            catch
+            {
+                if(serialPort1.IsOpen)
+                {
+                    serialPort1.Close();//如果是写数据是出错，此时窗口状态为开，就应关闭串口，防止下次不能使用。
+
+                }
+                MessageBox.Show("串口出错","错误提示");
+
+
+            }
+
+
+
+
+
+
+        }
+
+
+
     }
 }
